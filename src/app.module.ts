9264b62f753adc6f9da0f68436a6c42e99d03436
labibmuhajir <labibmuhajir@yahoo.com>
2024-08-28 14:04:26 +0700

    integrasi typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Book } from './book/book.entity';
 
 @Module({
-  imports: [BookModule],
+  imports: [
+    TypeOrmModule.forRoot(
+      {
+        type: 'sqlite',
+        database: 'perpustakaan.db',
+        entities: [Book],
+        synchronize: true
+      }
+    ),
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
